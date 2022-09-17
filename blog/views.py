dace49852a4a5ce54a3e1e29ab7d073d9b62c342
irenecoder <wairene60@gmail.com>
2022-09-17 12:12:12 +0300

    tag functionality

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,33 @@ from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView
 from .models import Post,Comment
 from .forms import EmailPostForm,CommentForm
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
 # Create your views here.
 
-# def post_list(request):
-#     posts = Post.published.all()
-#     return render(request,'blog/list.html',{'posts':posts})
+def post_list(request,tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag - get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list,3)#3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        #if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/list.html',{'posts':posts,'tag':tag})
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -70,32 +89,3 @@ def post_share(request, post_id):
                                                 'form': form,
                                                 'sent': sent})
 
-# def post_share(request,post_id):
-#     #retrieve post by id
-#     # post = get_object_or_404(Post, id=post_id, status='published')
-#     post = get_object_or_404(Post, id=post_id, status='published')
-#     sent = False
-
-#     if request.method == 'POST':
-#         #form was submitted
-#         form = EmailPostForm(request.POST)
-#         if form.is_valid():
-#             #Form fields passed validation
-#             cd = form.cleaned_data
-#             post_url = request.build_absolute_uri(
-#                         post.get_absolute_url())
-#             subject = f"{cd['name']} recommends you read " \
-#                 f"{post.title}"
-#             message = f"Read {post.title} at {post_url}\n\n" \
-#                     f"{cd['name']}\'s comments: {cd['comments']}"
-#             send_mail(subject, message, 'admin@myblog.com',
-#                         [cd['to']])
-#             sent = True
-
-#             #...send email
-#         else:
-#             form = EmailPostForm()
-#             context = {'post':post,'form':form,'sent':sent}
-#         return render(request,'blog/share.html',context)
-
-  
