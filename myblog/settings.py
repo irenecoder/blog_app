d86826c8c9052e16f5483126b7fe567874517194
irenecoder <wairene60@gmail.com>
2022-09-21 12:56:05 +0300

    postfeed and sitemap

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
     #blog app
     'blog',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
